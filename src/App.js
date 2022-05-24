21a0f87d3527a6fb47ea71ff2906b5814449b381
dvir455 <thedvirr@gmail.com>
2022-05-24 19:55:38 +0300

    switch added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { MainPage } from './pages/main-page';
+import { Switch, Route } from 'react-router-dom';
+import { HouseList } from './pages/house-list';
 
 function App() {
   return (
     <div className="App">
-     dsad
+      <Switch>
+        <Route path="/explore" component={HouseList} />
+        <Route path="/" component={MainPage} />
+      </Switch>
     </div>
   );
 }
